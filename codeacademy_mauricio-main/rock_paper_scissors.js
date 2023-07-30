75ce2b0d2e5c672fb669d849ea22464b1b2f86cc
MauzonX <mauricio.vcorreia@outlook.com>
2023-07-30 16:21:07 -0300

    voltei ao normal

--- codeacademy_mauricio-main/rock_paper_scissors.js
+++ codeacademy_mauricio-main/rock_paper_scissors.js
@@ -11,7 +11,7 @@ const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber){
     case 0:
-      return 'roc';
+      return 'rock';
     case 1:
       return 'paper';
     case 2:
