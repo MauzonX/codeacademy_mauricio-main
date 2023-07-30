024ce178739806db269931ee345d13d7d6cd797d
MauzonX <mauricio.vcorreia@outlook.com>
2023-07-30 16:30:42 -0300

    direto pro main

--- codeacademy_mauricio-main/rock_paper_scissors.js
+++ codeacademy_mauricio-main/rock_paper_scissors.js
@@ -3,7 +3,7 @@ const getUserChoice = userInput => {
   if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper' || userInput === 'bomb') {
     return userInput;
   } else {
-    console.log('PAREI'); 
+    console.log('PAREI direto pro main'); 
   } 
 }
 
